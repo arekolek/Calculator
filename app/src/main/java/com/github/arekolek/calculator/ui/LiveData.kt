757dd2dafbe58870b062ee1146c8826c6b45c627
Arek Olek <arekolek+git@gmail.com>
2022-09-03 13:56:21 +0200

    Fix warnings

--- app/src/main/java/com/github/arekolek/calculator/ui/LiveData.kt
+++
@@ -1,9 +0,0 @@
-package com.github.arekolek.calculator.ui
-
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, onChanged: (T) -> Unit) {
-    observe(owner, Observer(onChanged))
-}
